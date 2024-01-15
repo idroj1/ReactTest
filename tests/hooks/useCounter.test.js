b0a22f688656257faa1270a0d9b58bd489fb702e
idroj1 <mejiajordi@yahoo.com>
2024-01-15 15:25:46 -0400

    revised result

--- tests/hooks/useCounter.test.js
+++ tests/hooks/useCounter.test.js
@@ -1,10 +1,8 @@
-import { renderHook } from '@testing-library/react';
-import { useCounter } from "../../src/hooks/useCounter";
 
 describe('Pruebas en el useCounter', () => {
 
     test('debe de retornar los valores por defecto', () => {
-        const { result } = renderHook( () => useCounter() );
+        
     });
     
 })
